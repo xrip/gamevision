2d87a3317eae43eb070691fb3d47c9cc5198c22e
vrodin <mattywut@gmail.com>
2024-07-09 14:11:32 +0300

    fix screen position, first sound implementation on watara

--- cc65/main.c
+++ cc65/main.c
@@ -20,20 +20,25 @@ static void init(void)
 void main(void)
 {
     init();
+    memset(SV_VIDEO, 0, 0x8000);
+
     while (1)
     {
         SV_LCD.xpos = 0;
         SV_LCD.ypos = 0;
         SV_DMA_BUFFER.source = 0xC000;
-        SV_DMA_BUFFER.destination = (int)SV_VIDEO;
+        SV_DMA_BUFFER.destination = (int)SV_VIDEO + 0x180;
         SV_DMA_BUFFER.lenght = 0xD8;
         SV_DMA_BUFFER.control = 0x80;
 
         SV_DMA_BUFFER.source = 0xCD80;
-        SV_DMA_BUFFER.destination = (int)SV_VIDEO + 0xD80;
+        SV_DMA_BUFFER.destination = (int)SV_VIDEO + 0xF00;
         SV_DMA_BUFFER.lenght = 0xD8;
         SV_DMA_BUFFER.control = 0x80;
 
-        memcpy(SV_VIDEO + 0x1B00, (char *)(0xF000 | SV_CONTROL), 1);
+        SV_DMA.start = 0xC000 + 0x1E00;
+        SV_DMA.size = 0x55;
+        SV_DMA.control = 0b00001100;
+        SV_DMA.on = 0x80;
     }
 }
